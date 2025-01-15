385f6740cc051564a75a8798a742b2bf5a94522d
gisa chris <125574259+gisachris@users.noreply.github.com>
2025-01-15 11:18:25 +0200

    add logo image

--- prototype/secret_memory_prototype/project/app/components/Navigation.tsx
+++ prototype/secret_memory_prototype/project/app/components/Navigation.tsx
@@ -10,8 +10,8 @@ export default function Navigation() {
     <nav className="bg-zinc-900 border-b border-yellow-400/20 relative z-50">
       <div className="max-w-7xl mx-auto p-4">
         <div className="flex items-center justify-between">
-          <Link href="/" className="w-12 h-12 rounded-full bg-yellow-400 flex items-center justify-center">
-            <span className="text-black font-bold text-xl">SR</span>
+          <Link href="/" className="w-12 h-12 rounded-full bg-yellow-400 flex items-center justify-center overflow-hidden">
+            <img src="./logo.png" alt="logo" className="w-10 h-10 rounded-full" />
           </Link>
           
           {/* Desktop Navigation */}
